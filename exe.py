ea54be59568156a486a196af7bd3f27ba2fda168
Jottapanicx <103844471+Jottapanicx@users.noreply.github.com>
2026-06-19 14:09:37 -0300

    Add files via upload

---
+++ exe.py
@@ -0,0 +1,9 @@
+primeiro_valor = int(input("[*] Digite o primeiro valor: "))
+
+segundo_valor = int(input("[*] Digite o segundo valor: "))
+
+print(primeiro_valor * segundo_valor)
+print(primeiro_valor / segundo_valor)
+print(primeiro_valor ** segundo_valor)
+print(primeiro_valor + segundo_valor)
+print(primeiro_valor - segundo_valor)
