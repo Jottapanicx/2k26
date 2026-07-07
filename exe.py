8c56fe85356f2ce25eec81703e4260f6243179a8
Jottapanicx <103844471+Jottapanicx@users.noreply.github.com>
2026-07-06 23:54:08 -0300

    Add files via upload

---
+++ exe.py
@@ -0,0 +1,11 @@
+# Maquina de refrigerante
+
+refrigerantes = ["Coca cola", "Pepsi", "Fanta"]
+
+def menu():
+  print("MAQUINA DE REFRIGERANTES ")
+  for r in refrigerantes:
+    print("\n", r)
+  
+menu()
+  
